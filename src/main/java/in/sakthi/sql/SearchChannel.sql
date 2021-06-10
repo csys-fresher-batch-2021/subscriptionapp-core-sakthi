73f2f516956cc8186be01f857bb3cafa9c50540f
Sakthipriya V <buddysakthi14@gmail.com>
2021-06-10 15:39:29 +0530

    searchChannel

---
+++ src/main/java/in/sakthi/sql/SearchChannel.sql
@@ -0,0 +1,3 @@
+create table searchChannel (channelName varchar(40),channelId int,planName varchar(40),price int,validity varchar(40));
+insert into searchChannel(channelName,channelId,planName,price,validity) values ('Hotstar VIP',103,'Monthly',500,'30days');
+select * from searchChannel;
