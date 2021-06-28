d0f8a0a9b6be2acf5790afeaa5812d26f960609d
Sakthipriya V <buddysakthi14@gmail.com>
2021-06-28 18:58:12 +0530

    Discount

--- src/test/java/in/sakthi/TestDiscount.java
+++ src/test/java/in/sakthi/TestDiscount.java
@@ -1,15 +1,14 @@
 package in.sakthi;
 
 import in.sakthi.model.Discount;
-import in.sakthi.service.DiscountDate;
 import in.sakthi.service.DiscountService;
 
 public class TestDiscount {
 	public static void main(String[] args) {
 		String channelName = "Hotstar";
-		String validity = "Monthly";
+		String validity = "onthly";
 		Discount list = new Discount(channelName, validity);
 		DiscountService.discount(list);
-		DiscountDate.date(validity);
+		DiscountService.date(validity);
 	}
 }
