92b2c3db5e16df6dac43e9303b5f8b95813db829
Sakthipriya V <buddysakthi14@gmail.com>
2021-07-01 10:47:07 +0530

    Offer

--- src/main/java/in/sakthi/service/OfferService.java
+++ src/main/java/in/sakthi/service/OfferService.java
@@ -3,6 +3,7 @@ package in.sakthi.service;
 import java.sql.SQLException;
 import java.util.List;
 import in.sakthi.dao.OfferDao;
+import in.sakthi.exception.ServiceException;
 import in.sakthi.log.Logger;
 import in.sakthi.model.Offer;
 
@@ -13,11 +14,16 @@ public class OfferService {
 	 * @return
 	 * @throws SQLException
 	 */
-	public static List<Offer> getOffer() throws SQLException {
-		return OfferDao.getAllOffer();
+	public static List<Offer> getOffer() throws ServiceException {
+		try {
+			return OfferDao.getAllOffer();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws ServiceException {
 		List<Offer> offerList = OfferService.getOffer();
 		for (Offer listOffer : offerList) {
 			String obj = "Offer is: " + listOffer;
